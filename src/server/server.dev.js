7636c8ade28d5a04656e4129c5ffef0a3e99a850
DH <dmitropetr@gmail.com>
2017-05-10 23:31:58 +0300

    heroku fix 9

--- src/server/server.dev.js
+++ src/server/server.dev.js
@@ -81,12 +81,12 @@ app.get('/*', function(req, res) {
     });
 });
 
-const server = app.listen(43760, (err) => {
+const server = app.listen(app.get('port'), 'localhost', (err) => {
     if (err) {
         console.log(err);
         return;
     }
-    console.log('Dev server listening on port: ', 43760);
+    console.log('Dev server listening on port: ', app.get('port'));
 });
 
 const io = new SocketIo(server, {path: '/api/chat'});
