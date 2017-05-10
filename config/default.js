2797d7fefecc83bef347c514db6da8c5ef057f81
DH <dmitropetr@gmail.com>
2017-05-10 17:05:43 +0300

    add mongolab link

--- config/default.js
+++ config/default.js
@@ -1,12 +1,14 @@
 import path from 'path';
 
 module.exports = {
-    port: 3000,
-    siteHost: 'http://localhost:3000',
+    port: process.env.PORT || 3001,
+    siteHost: 'http://localhost:3001',
     secret: 'mysecret',
     root: process.cwd(),
     mongoose: {
-        uri: 'mongodb://localhost/chatapp',
+        uri: 'mongodb://hdmi:1234@ds137271.mlab.com:37271/hdmiapp',
+        // uri: 'mongodb://localhost/chatapp',
+
         options: {
             server: {
                 poolSize: 5,
